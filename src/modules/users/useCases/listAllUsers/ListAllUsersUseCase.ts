b1074c648c64a78d41761e25a11afa2b5bb1d408
Edio Gama <ediogama@hotmail.com>
2021-03-26 16:53:31 -0300

    PASS ALL TESTS

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,14 +2,26 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+  user_id: string | string[];
 }
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userAdmin = this.usersRepository.findById(user_id);
+
+    if (!userAdmin) {
+      throw new Error("User not found");
+    }
+
+    if (!userAdmin.admin) {
+      throw new Error("User is not an admin");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
